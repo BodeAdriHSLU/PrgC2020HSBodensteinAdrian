0ee3538c113ef59870eddcfe650ab891554c9519
BodeAdriHSLU <adrian.bodenstein@gmail.com>
2020-09-23 12:07:41 +0200

    Testatübung 1

---
+++ 30_Exercises/SW02/numberToText.c
@@ -0,0 +1,72 @@
+/*
+Team:
+Raul Kiener
+Marc Wenger
+Adrian Bodenstein
+*/
+
+
+#include <stdio.h>
+
+int main( int argc, char* argv[] )
+{
+	int readValue, stopValue = 0;
+	
+	while( stopValue == 0 )
+	{
+	printf("Enter a number between (including) 0 and 9 to display the number as text. Enter 10 to stop the program: ");
+	scanf("%d", &readValue);
+		switch(readValue)
+		{
+			case 0:
+				printf("0 as text is 'zero'\n");
+				break;
+			
+			case 1:
+				printf("1 as text is 'one'\n");
+				break;
+			
+			case 2:
+				printf("2 as text is 'two'\n");
+				break;
+			
+			case 3:
+				printf("3 as text is 'three'\n");
+				break;
+		
+			case 4:
+				printf("4 as text is 'four'\n");
+				break;
+			
+			case 5:
+				printf("5 as text is 'five'\n");
+				break;
+			
+			case 6:
+				printf("6 as text is 'six'\n");
+				break;
+			
+			case 7:
+				printf("7 as text is 'seven'\n");
+				break;
+			
+			case 8:
+				printf("8 as text is 'eight'\n");
+				break;
+			
+			case 9:
+				printf("9 as text is 'nine'\n");
+				break;
+			
+			case 10:
+				printf("Thank you. Good bye.\n");
+				stopValue++;
+				break;
+			
+			default:
+				printf("invalid -> try again\n");
+				break;
+		}	
+	}	
+	return 0;
+}
